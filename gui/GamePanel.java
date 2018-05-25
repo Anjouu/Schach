7975c2e18f1716493ed33c60ea2e44da357cf2a8
Unknown <finn.eggers@gmx.de>
2018-05-25 11:27:38 +0200

    123

--- gui/GamePanel.java
+++ gui/GamePanel.java
@@ -137,7 +137,7 @@ public class GamePanel extends JPanel{
                     if(z.getX_from() == selected_x && z.getY_from() == selected_y && z.getX_to() == x && z.getY_to() == y) {
                         g.move(z);
 
-                        g.move(KI.getBestMove(g,6));
+                        g.move(KI.getBestMove(g,4));
 
                         selected_x = -1;
                         break;
